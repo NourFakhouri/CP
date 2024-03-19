e0a2fc890c43081155b796bbddfa9be9a187205d
Nour Al-Deen <113638219+NourFakhouri@users.noreply.github.com>
2024-03-19 20:21:33 +0600

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+const ll N = (ll) 2e5+9;
+const ll oo = (ll) 1e18;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+void solve()
+{
+  
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    ll tt = 1;
+    cin >> tt;
+    while(tt--)
+    {
+    	//cout << "Case #" << tt << " : \n";
+    	solve();
+    }
+    return 0;
+}
